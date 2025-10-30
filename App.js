5253d30f8702c62df50d7d151a4212eeb51c3040
Pratap singh <pratapsi356@gmail.com>
2025-10-30 10:55:56 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🚀 React App Dockerized with Multi-Stage Build</h1>
+      <p>This React app is served via Nginx from a lightweight Docker image!</p>
+    </div>
+  );
+}
+
+export default App;
